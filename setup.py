762ec513de45dcc6b23a7410e041f13389817b28
Yuri Shkuro <ys@uber.com>
2016-07-11 13:24:20 -0400

    Bump snapshot version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='basictracer',
-    version='2.0.0.dev0',
+    version='2.0.0.dev1',
     author='Brandon Gonzalez, The OpenTracing Authors',
     author_email='bg@lightstep.com',
     license='MIT',
