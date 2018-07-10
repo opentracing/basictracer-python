092a28c0802dcd96f25198563019f1434ba2aa5e
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2018-07-10 18:41:46 +0200

    Back to development: 3.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='basictracer',
-    version='3.0.0',
+    version='3.0.1.dev0',
     author='The OpenTracing Authors',
     author_email='info@opentracing.io',
     license='MIT',
