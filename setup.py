46488337dbafae973b602604de7b4164de8d84e5
Yuri Shkuro <ys@uber.com>
2019-05-12 16:12:51 -0400

    Back to development: 3.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='basictracer',
-    version='3.1.0',
+    version='3.1.1.dev0',
     author='The OpenTracing Authors',
     author_email='info@opentracing.io',
     license='MIT',
